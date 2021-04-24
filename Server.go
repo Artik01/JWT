36d758f284cd3ce369f556443ab3b48786473d6a
Artik01 <xlazartx@gmail.com>
2021-04-24 15:16:47 +0300

    fixed bug with mac check

--- Server.go
+++ Server.go
@@ -123,7 +123,7 @@ func ValidMAC(message, messageMAC, key []byte) bool {
 	mac := hmac.New(sha256.New, key)
 	mac.Write(message)
 	expectedMAC := mac.Sum(nil)
-	return hmac.Equal(messageMAC, expectedMAC)
+	return hmac.Equal(messageMAC, []byte(Encoder.EncodeToString(expectedMAC)))
 }
 
 func GetExp(t string) time.Time {
